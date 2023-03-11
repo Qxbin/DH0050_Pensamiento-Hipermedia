49b580734d1fa88c05c4fcbb7beb07b450d5a7c9
Qxbin <126956928+Qxbin@users.noreply.github.com>
2023-03-11 10:40:27 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,29 +1,54 @@
-let angles = [30, 10, 45, 35, 60, 38, 75, 67];
+// Where is the car
+let x, y;
+
+
+class Carro {
+  constructor(alto,ancho,color,name){
+    this.alto = alto;
+    this.ancho = ancho;
+    this.color = color;
+    this.name = name;
+  } 
+}    
+
+
 
 function setup() {
-  createCanvas(720, 400);
-  noStroke();
-  noLoop(); // Corre una vez y luego para.
+ 
+  createCanvas(400, 200);
+  // Starts in the middle
+  y = height - height/4;
+  x = 0;
+  let miCarro = new Carro (10,20,(red),"Elcirveralcon");
+  
+  console.log("miCarro anchoh is: " + miCarro.ancho);
+  console.log("miCarro alto is: " + miCarro.alto);
+  console.log("Nombre de miCarro es: " + miCarro.name);
 }
 
 function draw() {
-  background(100);
-  pieChart(300, angles);
+  background(0);  
+  display();
+  move();
 }
 
-function pieChart(diameter, data) {
-  let lastAngle = 0;
-  for (let i = 0; i < data.length; i++) {
-    let gray = map(i, 0, data.length, 0, 255);
-    fill(gray);
-    arc(
-      width / 2,
-      height / 2,
-      diameter,
-      diameter,
-      lastAngle,
-      lastAngle + radians(angles[i])
-    );
-    lastAngle += radians(angles[i]);
+function move(){
+  // Jiggling randomly
+  y = y + random(-2, 2);
+  
+  if (x > width) {
+    // Reset 
+    x = 0;
+      y = height - height/4;
+  } else {
+    // Moving up at a constant speed
+    x = x + 3;
   }
+}
+
+function display(){
+  // Draw a car
+  stroke(50);
+  fill(223,124,80);
+  rect(x, y, 40, 20);
 }
