ce4e5c05dee30bedbb64e281df22382a0c83c01c
Qxbin <126956928+Qxbin@users.noreply.github.com>
2023-03-18 10:55:17 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -4,91 +4,98 @@ let supraY, gtrY;
 let speed = 4;
 let jumping = false;
 let jumpCounter = 0;
+let collisions = 0;
 
 class Carro {
-  constructor(alto, ancho, color, name) {
-    this.alto = alto;
-    this.ancho = ancho;
-    this.color = color;
-    this.name = name;
-  }
+constructor(alto, ancho, color, name) {
+this.alto = alto;
+this.ancho = ancho;
+this.color = color;
+this.name = name;
+}
 
-  display(x, y) {
-    stroke(220, 200, 200);
-    fill(this.color);
-    rect(x, y, this.ancho, this.alto);
-  }
+display(x, y) {
+stroke(220, 200, 200);
+fill(this.color);
+rect(x, y, this.ancho, this.alto);
+}
 }
 
 function setup() {
-  createCanvas(400, 200);
-  supraX = 0;
-  gtrX = width;
-  supraY = height - height / 4;
-  gtrY = height - height / 4;
+createCanvas(400, 200);
+supraX = 0;
+gtrX = width;
+supraY = height - height / 4;
+gtrY = height - height / 4;
 
-  supra = new Carro(20, 40, "blue", "supra");
-  gtr = new Carro(20, 40, "orange", "gtr");
+supra = new Carro(20, 40, "blue", "supra");
+gtr = new Carro(20, 40, "orange", "gtr");
 
-  console.log("supra ancho es " + supra.ancho);
-  console.log("supra alto es " + supra.alto);
-  console.log("supra color es " + supra.color);
-  console.log("supra name es " + supra.name);
+console.log("supra ancho es " + supra.ancho);
+console.log("supra alto es " + supra.alto);
+console.log("supra color es " + supra.color);
+console.log("supra name es " + supra.name);
 
-  console.log("gtr ancho es " + gtr.ancho);
-  console.log("gtr alto es " + gtr.alto);
-  console.log("gtr color es " + gtr.color);
-  console.log("gtr name es " + gtr.name);
+console.log("gtr ancho es " + gtr.ancho);
+console.log("gtr alto es " + gtr.alto);
+console.log("gtr color es " + gtr.color);
+console.log("gtr name es " + gtr.name);
 }
 
 function draw() {
-  background(0);
+background(0);
 
-  supraX += speed;
-  gtrX -= speed;
+supraX += speed;
+gtrX -= speed;
 
-  if (supraX > width - supra.ancho) {
-    supraX = width - supra.ancho;
-    speed * -1;
-  } else if (supraX < 0) {
-    supraX = 0;
-    speed * -1;
-  }
+if (supraX > width - supra.ancho) {
+supraX = width - supra.ancho;
+speed * -1;
+} else if (supraX < 0) {
+supraX = 0;
+speed * -1;
+}
 
-  if (gtrX < gtr.ancho) {
-    gtrX = gtr.ancho;
-    speed *= -1;
-  } else if (gtrX > width) {
-    gtrX = width;
-    speed *= -1;
-  }
+if (gtrX < gtr.ancho) {
+gtrX = gtr.ancho;
+speed *= -1;
+} else if (gtrX > width) {
+gtrX = width;
+speed *= -1;
+}
 
-  if (jumping) {
-    if (jumpCounter < 10) {
-      supraY -= 5;
-    } else if (jumpCounter < 20) {
-      supraY += 5;
-    } else {
-      jumping = false;
-    }
-    jumpCounter++;
-  } else {
-    supraY = height - height / 4;
-  }
+if (jumping) {
+if (jumpCounter < 10) {
+supraY -= 5;
+} else if (jumpCounter < 20) {
+supraY += 5;
+} else {
+jumping = false;
+}
+jumpCounter++;
+} else {
+supraY = height - height / 4;
+}
 
-  supra.display(supraX, supraY);
-  gtr.display(gtrX, gtrY);
+supra.display(supraX, supraY);
+gtr.display(gtrX, gtrY);
+
+// Comprobar colisiones
+if (supraX + supra.ancho >= gtrX && supraX <= gtrX + gtr.ancho && supraY + supra.alto >= gtrY && supraY <= gtrY + gtr.alto) {
+collisions++;
+console.log("Colisiones: " + collisions);
+}
 }
 
 function salto() {
-  if (!jumping) {
-    jumping = true;
-    jumpCounter = 0;
-  }
+if (!jumping) {
+jumping = true;
+jumpCounter = 0;
+}
 }
 
 function keyPressed() {
-  if (keyCode === 32) {
-    salto();
-  }
+if (keyCode === 32) {
+salto();
+}
 }
