365225efa371a5ae77ee57342d1e07393d65104d
Qxbin <126956928+Qxbin@users.noreply.github.com>
2023-03-18 10:39:35 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,54 +1,94 @@
-// Where is the car
-let x, y;
-
+let supra, gtr;
+let supraX, gtrX;
+let supraY, gtrY;
+let speed = 4;
+let jumping = false;
+let jumpCounter = 0;
 
 class Carro {
-  constructor(alto,ancho,color,name){
+  constructor(alto, ancho, color, name) {
     this.alto = alto;
     this.ancho = ancho;
     this.color = color;
     this.name = name;
-  } 
-}    
+  }
 
-
-
-function setup() {
- 
-  createCanvas(400, 200);
-  // Starts in the middle
-  y = height - height/4;
-  x = 0;
-  let miCarro = new Carro (10,20,(red),"Elcirveralcon");
-  
-  console.log("miCarro anchoh is: " + miCarro.ancho);
-  console.log("miCarro alto is: " + miCarro.alto);
-  console.log("Nombre de miCarro es: " + miCarro.name);
-}
-
-function draw() {
-  background(0);  
-  display();
-  move();
-}
-
-function move(){
-  // Jiggling randomly
-  y = y + random(-2, 2);
-  
-  if (x > width) {
-    // Reset 
-    x = 0;
-      y = height - height/4;
-  } else {
-    // Moving up at a constant speed
-    x = x + 3;
+  display(x, y) {
+    stroke(220, 200, 200);
+    fill(this.color);
+    rect(x, y, this.ancho, this.alto);
   }
 }
 
-function display(){
-  // Draw a car
-  stroke(50);
-  fill(223,124,80);
-  rect(x, y, 40, 20);
+function setup() {
+  createCanvas(400, 200);
+  supraX = 0;
+  gtrX = width;
+  supraY = height - height / 4;
+  gtrY = height - height / 4;
+
+  supra = new Carro(20, 40, "blue", "supra");
+  gtr = new Carro(20, 40, "orange", "gtr");
+
+  console.log("supra ancho es " + supra.ancho);
+  console.log("supra alto es " + supra.alto);
+  console.log("supra color es " + supra.color);
+  console.log("supra name es " + supra.name);
+
+  console.log("gtr ancho es " + gtr.ancho);
+  console.log("gtr alto es " + gtr.alto);
+  console.log("gtr color es " + gtr.color);
+  console.log("gtr name es " + gtr.name);
+}
+
+function draw() {
+  background(0);
+
+  supraX += speed;
+  gtrX -= speed;
+
+  if (supraX > width - supra.ancho) {
+    supraX = width - supra.ancho;
+    speed * -1;
+  } else if (supraX < 0) {
+    supraX = 0;
+    speed * -1;
+  }
+
+  if (gtrX < gtr.ancho) {
+    gtrX = gtr.ancho;
+    speed *= -1;
+  } else if (gtrX > width) {
+    gtrX = width;
+    speed *= -1;
+  }
+
+  if (jumping) {
+    if (jumpCounter < 10) {
+      supraY -= 5;
+    } else if (jumpCounter < 20) {
+      supraY += 5;
+    } else {
+      jumping = false;
+    }
+    jumpCounter++;
+  } else {
+    supraY = height - height / 4;
+  }
+
+  supra.display(supraX, supraY);
+  gtr.display(gtrX, gtrY);
+}
+
+function salto() {
+  if (!jumping) {
+    jumping = true;
+    jumpCounter = 0;
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    salto();
+  }
 }
